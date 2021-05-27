8d9ed902e894c11283889c1559f356d752a72663
0rv3 <11827620+Orve@users.noreply.github.com>
2021-05-28 04:26:54 +0900

    一旦色々コミット

--- JavaScript/page.js
+++ JavaScript/page.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors: ['1page', '2page', '3page','4page','5page','6page'],
         menu: '#menu',
-        sectionsColor: ['#AACF52', '#4BBFC3', '#7BAABE', '#E5E5E5','#7BAABE','#E5E5E5']
+        // sectionsColor: ['#AACF52', '#4BBFC3', '#7BAABE', '#E5E5E5','#7BAABE','#E5E5E5']
         });
 });
+
+
